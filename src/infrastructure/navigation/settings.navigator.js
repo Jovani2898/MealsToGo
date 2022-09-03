bb3de109680c579fb89d11070f4857210d17fa3e
Jovani2898 <jovani2898@mail.ru>
2022-09-03 18:42:24 +0400

    change favourites

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { SettingScreen } from "../../features/settings/screens/settings.screen";
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 import { CameraScreen } from "../../features/settings/screens/camera.screen";
 
 import {
   createStackNavigator,
   CardStyleInterpolators,
-} from "@react-navigation//stack";
+} from "@react-navigation/stack";
 
 const SettingsStack = createStackNavigator();
 
@@ -23,7 +23,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
           header: () => null,
         }}
         name="Settings"
-        component={SettingScreen}
+        component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
       <SettingsStack.Screen name="Camera" component={CameraScreen} />
